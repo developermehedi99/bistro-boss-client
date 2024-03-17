d46ddf73cb097021670f4edcd45f0937fdcbbac6
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-03-17 23:25:27 +0600

    frist

---
+++ src/shared/Navbar.jsx
@@ -0,0 +1,62 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const navLinks = (
+    <>
+      <li>
+        <Link to="/">HOME</Link>
+      </li>
+      <li>
+        <Link to="/contact">CONTACT US</Link>
+      </li>
+      <li>
+        <Link to="/dashboard">DASHBOARD</Link>
+      </li>
+      <li>
+        <Link to="/ourmenu">OUR MENU</Link>
+      </li>
+      <li>
+        <Link to="/shop">OUR SHOP</Link>
+      </li>
+      <li>
+        <Link to="/singUp">SING OUT</Link>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar bg-black text-white fixed z-10 max-w-7xl ">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navLinks}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">BISTROBOSS</a>
+      </div>
+      <div className="navbar-end hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navLinks}</ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
